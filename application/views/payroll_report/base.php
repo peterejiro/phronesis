f56eaf98ca902b4f9bdda09ca0123dfa2e3a8260
peterejiro <peterejiro96@gmail.com>
2020-06-25 09:32:49 +0100

    -- datatable, drop zone, fixed

--- application/views/payroll_report/base.php
+++ application/views/payroll_report/base.php
@@ -18,81 +18,58 @@
 
 
 		<div class="main-content">
+
 			<section class="section">
 				<div class="section-header">
-					<h1> PayRoll Reports</h1>
+					<h1>PayRoll Reports</h1>
 				</div>
 
-				<div class="row">
-					<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-					<a href="<?php echo site_url('emolument'); ?>">
-						<div class="card card-statistic-1">
-							<div class="card-icon bg-primary">
-								<i class="far fa-user"></i>
-							</div>
-							<div class="card-wrap">
-								<div class="card-header">
-<!--									<h4>Total Employees</h4>-->
+				<div class="section-body">
+
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="card card-large-icons">
+								<div class="card-icon bg-primary text-white">
+									<i class="fas fa-cog"></i>
 								</div>
 								<div class="card-body">
-									Emolument Report
+									<h4>Emolument Report</h4>
+
+									<a href="<?php echo base_url('emolument') ?>" class="card-cta">View Report <i class="fas fa-chevron-right"></i></a>
 								</div>
 							</div>
 						</div>
-					</a>
-					</div>
-					<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-						<a href="<?php echo site_url('deduction'); ?>">
-							<div class="card card-statistic-1">
-								<div class="card-icon bg-primary">
-									<i class="far fa-user"></i>
-								</div>
-								<div class="card-wrap">
-									<div class="card-header">
-<!--										<h4>Total Employees</h4>-->
-									</div>
-									<div class="card-body">
-										Deduction
-									</div>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-						<a href="<?php echo site_url('pay_order'); ?>">
-							<div class="card card-statistic-1">
-								<div class="card-icon bg-primary">
-									<i class="far fa-user"></i>
-								</div>
-								<div class="card-wrap">
-									<div class="card-header">
-<!--										<h4>Total Employees</h4>-->
-									</div>
-									<div class="card-body">
-										Pay Order
-									</div>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-3 col-md-6 col-sm-6 col-12">
-						<div class="card card-statistic-1">
-							<div class="card-icon bg-success">
-								<i class="fas fa-circle"></i>
-							</div>
-							<div class="card-wrap">
-								<div class="card-header">
-									<h4>Online Users</h4>
+						<div class="col-lg-6">
+							<div class="card card-large-icons">
+								<div class="card-icon bg-primary text-white">
+									<i class="fas fa-search"></i>
 								</div>
 								<div class="card-body">
-									47
+									<h4>Deduction Report</h4>
+
+									<a href="<?php echo base_url('deduction'); ?>" class="card-cta">View Report <i class="fas fa-chevron-right"></i></a>
 								</div>
 							</div>
 						</div>
+						<div class="col-lg-6">
+							<div class="card card-large-icons">
+								<div class="card-icon bg-primary text-white">
+									<i class="fas fa-envelope"></i>
+								</div>
+								<div class="card-body">
+									<h4>Pay Order</h4>
+
+									<a href="<?php echo base_url('pay_order'); ?>" class="card-cta">View Report <i class="fas fa-chevron-right"></i></a>
+								</div>
+							</div>
+						</div>
+
 					</div>
 				</div>
-
 			</section>
+
+
+
 		</div>
 
 
