c616c5b1e13504e4460d2bdd6df19b2490b3a3ea
cjamanambu <oamanambu@yahoo.com>
2020-07-10 10:30:46 +0100

    UI chnage to qualification setup page

--- application/views/hr_config/qualification.php
+++ application/views/hr_config/qualification.php
@@ -1,174 +1,134 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<?php include(APPPATH.'\views\stylesheet.php'); ?>
 
-	<?php include(APPPATH.'\views\stylesheet.php');
-
-	?>
-	<!-- DataTables -->
-
-
-</head>
-
-
-<body class="fixed-left">
-<!-- Begin page -->
+<body>
 <div id="app">
 	<div class="main-wrapper">
 		<div class="navbar-bg"></div>
 		<?php include(APPPATH.'\views\topbar.php'); ?>
-
-
 		<?php include(APPPATH.'\views\sidebar.php'); ?>
-
-
-
 		<div class="main-content">
 			<section class="section">
 				<div class="section-header">
-
 					<h1>Qualification Setup</h1>
-
+          <div class="section-header-breadcrumb">
+            <div class="breadcrumb-item active"><a href="<?php echo base_url(); ?>">Dashboard</a></div>
+            <div class="breadcrumb-item">Qualification Setup</div>
+          </div>
 				</div>
-
-
-				<div class="row">
-					<div class="col-12 col-md-12 col-lg-12">
-						<div class="card">
-							<div class="card-body">
-								<button class="btn btn-secondary btn-round" type="button"data-toggle="modal" data-target="#add_qualification" aria-haspopup="true" aria-expanded="false" style="margin: 5vh">
-									<i class="mdi mdi-new-box "></i>Add New qualification
-								</button>
-
-
-
-								<table id="datatable-buttons" class="table table-bordered table-md">
-									<thead>
-
-
-
-									<tr>
-										<th>Name of qualification</th>
-										<th>Action</th>
-
-									</tr>
-									</thead>
-
-
-									<tbody>
-									<?php if(!empty($qualifications)):
-										foreach($qualifications as $qualification):
-											?>
-											<tr>
-												<td><?php echo $qualification->qualification_name; ?></td>
-												<td> <button type="button" class="btn btn-primary m-b-10 m-l-10 waves-effect waves-light" data-toggle="modal" data-target="#edit_qualification<?php echo $qualification->qualification_id ?>">
-														<i class="mdi mdi-table-edit "></i>
-													</button>
-													<button type="button" class="btn btn-danger m-b-10 m-l-10 waves-effect waves-light">
-														<i class="mdi mdi-delete-forever "></i>
-													</button></td>
-
-											</tr>
-
-										<?php
-
-										endforeach;
-									endif; ?>
-
-									</tbody>
-								</table>
-
-
-
-
-							</div>
-
-
-						</div>
-					</div>
-
-
-
-
-				</div>
-
+        <div class="section-body">
+          <div class="section-title">All About Qualification Setup</div>
+          <p class="section-lead">You can manage educational qualification information here</p>
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <h4>All Qualifications</h4>
+                  <div class="card-header-action">
+                    <button data-toggle="modal" data-target="#add_qualification" type="button" class="btn btn-icon icon-left btn-primary"><i class="fa fa-plus"></i> Add Qualification</button>
+                  </div>
+                </div>
+                <div class="card-body">
+                 <div class="table-responsive">
+                   <table id="datatable-buttons" class="table table-striped table-bordered table-md">
+                     <thead>
+                       <tr>
+                         <th>Qualification</th>
+                         <th>Actions</th>
+                       </tr>
+                     </thead>
+                     <tbody>
+		                 <?php if(!empty($qualifications)):
+			                 foreach($qualifications as $qualification):
+				                 ?>
+                         <tr>
+                           <td><?php echo $qualification->qualification_name; ?></td>
+                           <td class="text-center" style="width: 9px">
+                             <div class="dropdown">
+                               <a href="#" data-toggle="dropdown"><i class="fas fa-ellipsis-h"></i></a>
+                               <div class="dropdown-menu">
+                                 <a class="dropdown-item has-icon" href="#" data-toggle="modal" data-target="#edit_qualification<?php echo $qualification->qualification_id ?>"><i class="fas fa-edit"></i>Edit Bank</a>
+                               </div>
+                             </div>
+                           </td>
+                         </tr>
+			                 <?php
+			                 endforeach;
+		                 endif; ?>
+                     </tbody>
+                   </table>
+                 </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 			</section>
 			<div class="modal fade" id="add_qualification" tabindex="-1" role="dialog" aria-hidden="true">
 				<div class="modal-dialog modal-dialog-centered" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
-							<h5 class="modal-title" id="exampleModalLongTitle2">Add New qualification</h5>
+							<h5 class="modal-title" id="exampleModalLongTitle2">Add New Qualification</h5>
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 								<span aria-hidden="true" class="text-dark">&times;</span>
 							</button>
 						</div>
-						<form class="" method="post" action="<?php echo site_url('add_qualification'); ?>">
+						<form class="needs-validation" novalidate method="post" action="<?php echo site_url('add_qualification'); ?>">
 							<div class="modal-body">
-
 								<div class="form-group">
-									<label>Name of qualification:</label>
-									<input type="text" class="form-control"  name="qualification_name" required placeholder="Enter Name of qualification"/>
+									<label>Qualification</label><span style="color: red"> *</span>
+									<input type="text" class="form-control"  name="qualification_name" required/>
+                  <div class="invalid-feedback">
+                    please fill in a qualification
+                  </div>
 								</div>
-
-
 								<input type="hidden" name="<?php echo $csrf_name;?>" value="<?php echo $csrf_hash;?>" />
-
-
 							</div>
-							<div class="modal-footer">
-								<button type="submit" class="btn btn-primary">Add New qualification</button>
-								<button type="reset" class="btn btn-danger ml-2" data-dismiss="modal">Close</button>
-							</div>
+              <div class="modal-footer bg-whitesmoke">
+                <button type="submit" class="btn btn-primary">Add Qualification</button>
+                <input type="reset" class="btn btn-secondary">
+                <button type="reset" class="btn btn-danger" data-dismiss="modal">Close</button>
+              </div>
 						</form>
 					</div>
 				</div>
 			</div>
-
 
 			<?php foreach($qualifications as $qualification): ?>
 				<div class="modal fade" id="edit_qualification<?php echo $qualification->qualification_id ?>" tabindex="-1" role="dialog" aria-hidden="true">
 					<div class="modal-dialog modal-dialog-centered" role="document">
 						<div class="modal-content">
 							<div class="modal-header">
-								<h5 class="modal-title" id="exampleModalLongTitle2">Update qualification</h5>
+								<h5 class="modal-title" id="exampleModalLongTitle2">Edit Qualification</h5>
 								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 									<span aria-hidden="true" class="text-dark">&times;</span>
 								</button>
 							</div>
-							<form class="" method="post" action="<?php echo site_url('update_qualification'); ?>">
+							<form class="needs-validation" novalidate method="post" action="<?php echo site_url('update_qualification'); ?>">
 								<div class="modal-body">
-
 									<div class="form-group">
-										<label>Name of qualification:</label>
-										<input type="text" class="form-control"  name="qualification_name" required value="<?php echo $qualification->qualification_name; ?>" placeholder="Enter Name of qualification"/>
+                    <label>Qualification</label><span style="color: red"> *</span>
+                    <input type="text" class="form-control"  name="qualification_name" required value="<?php echo $qualification->qualification_name; ?>"/>
+                    <div class="invalid-feedback">
+                      please fill in a qualification
+                    </div>
 									</div>
-
-
 									<input type="hidden" name="<?php echo $csrf_name;?>" value="<?php echo $csrf_hash;?>" />
-
 									<input type="hidden" name="qualification_id" value="<?php echo $qualification->qualification_id;?>" />
-
-
 								</div>
-								<div class="modal-footer">
-									<button type="submit" class="btn btn-primary">Update qualification</button>
-									<button type="reset" class="btn btn-danger ml-2" data-dismiss="modal">Close</button>
-								</div>
+                <div class="modal-footer bg-whitesmoke">
+                  <button type="submit" class="btn btn-primary">Edit Qualification</button>
+                  <input type="reset" class="btn btn-secondary">
+                  <button type="reset" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
 							</form>
 						</div>
 					</div>
 				</div>
-
 			<?php endforeach; ?>
+    </div>
 	</div>
-
 </div>
-<!-- END wrapper -->
-
-
+<?php include(APPPATH.'\views\footer.php'); ?>
 <?php include(APPPATH.'\views\js.php'); ?>
-
-
 </body>
 </html>
-
