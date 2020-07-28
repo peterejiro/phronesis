3b1c1bd778f129ae70ce78c4f83a18d165e6c44d
CJ Amanambu <oamanambu@yahoo.com>
2020-07-28 12:19:14 +0100

    ui changes biometrics module

--- application/views/biometrics/present_employee.php
+++ application/views/biometrics/present_employee.php
@@ -9,33 +9,35 @@
 		<div class="main-content">
 			<section class="section">
 				<div class="section-header">
-					<h1>Employees Present On <?php echo $date; ?></h1>
+          <div class="section-header-back">
+            <a href="<?php echo site_url('present_employee')?>" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+          </div>
+					<h1>Present Employees Sheet</h1>
           <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="<?php echo base_url(); ?>">Dashboard</a></div>
-			  <div class="breadcrumb-item active"><a href="<?php echo base_url('biometrics_report'); ?>">Biometrics Report</a></div>
-            <div class="breadcrumb-item">Employee Present On <?php echo $date; ?></div>
+            <div class="breadcrumb-item active"><a href="<?php echo base_url('biometrics_report'); ?>">Attendance Reports</a></div>
+            <div class="breadcrumb-item active"><a href="<?php echo base_url('present_employee'); ?>">Present Employees</a></div>
+            <div class="breadcrumb-item">Present Employees Sheet</div>
           </div>
 				</div>
         <div class="section-body">
-          <div class="section-title">All About Employees Present On <?php echo $date; ?>  </div>
-          <p class="section-lead">You can View Employees Present On <?php echo $date; ?> </p>
+          <div class="section-title">All About Employees Present On <?php echo date('F j, Y', strtotime($date)); ?></div>
+          <p class="section-lead">You can view all employees present on the given date here</p>
           <div class="row">
             <div class="col-12">
               <div class="card">
                 <div class="card-header">
-                  <h4>All Employees Present On <?php echo $date; ?></h4>
-
+                  <h4>All Present Employees</h4>
                 </div>
                 <div class="card-body">
                   <div class="table-responsive">
-                    <table id="datatable-buttons" class="table table-bordered table-striped table-md">
+                    <table id="datatable-buttons-2" class="table table-bordered table-striped table-md">
                       <thead>
                       <tr>
                         <th>Employee Name</th>
-						  <th>Employee Unique Id</th>
+						            <th>Employee Unique Id</th>
                         <th>Clock In Time</th>
-						 <th> Date </th>
-
+						            <th>Date</th>
                       </tr>
                       </thead>
                       <tbody>
@@ -44,10 +46,9 @@
                           ?>
                           <tr>
                             <td><?php echo $present_employee->employee_last_name." ".$present_employee->employee_first_name." ".$present_employee->employee_other_name; ?></td>
-                           <td> <?php echo $present_employee->employee_unique_id; ?></td>
-                            <td><?php echo $present_employee->employee_biometrics_login_time; ?></td>
-							  <td> <?php echo date('Y-m-d', time()); ?></td>
-
+                            <td> <?php echo $present_employee->employee_unique_id; ?></td>
+                            <td><?php echo date('g:i a', strtotime($present_employee->employee_biometrics_login_time)); ?></td>
+							              <td> <?php echo date('F j, Y', time()); ?></td>
                           </tr>
                         <?php
                         endforeach;
@@ -56,7 +57,9 @@
                     </table>
                   </div>
                 </div>
-                <div class="card-footer bg-whitesmoke"></div>
+                <div class="card-footer text-right bg-whitesmoke">
+                  <button class="btn btn-danger" onclick="location.href='<?php echo site_url('present_employee');?>'">Go Back</button>
+                </div>
               </div>
             </div>
           </div>
@@ -69,3 +72,6 @@
 <?php include(APPPATH.'/views/js.php'); ?>
 </body>
 </html>
+<script>
+  $('title').html('Present Employees Sheet - IHUMANE');
+</script>
--- application/views/biometrics/today_absent.php
+++ application/views/biometrics/today_absent.php
@@ -1,7 +1,6 @@
 <?php include(APPPATH.'/views/stylesheet.php');
 $CI =& get_instance();
 $CI->load->model('biometric');
-
 ?>
 
 <body>
@@ -13,82 +12,69 @@ $CI->load->model('biometric');
 		<div class="main-content">
 			<section class="section">
 				<div class="section-header">
+          <div class="section-header-back">
+            <a href="<?php echo site_url('biometrics_report')?>" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+          </div>
 					<h1>Employees Absent today </h1>
           <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="<?php echo base_url(); ?>">Dashboard</a></div>
-			  <div class="breadcrumb-item active"><a href="<?php echo base_url('biometrics_report'); ?>">Biometrics Report</a></div>
+            <div class="breadcrumb-item active"><a href="<?php echo base_url('biometrics_report'); ?>">Attendance Reports</a></div>
             <div class="breadcrumb-item">Employee Absent Today</div>
           </div>
 				</div>
         <div class="section-body">
           <div class="section-title">All About Employees Absent Today</div>
-          <p class="section-lead">You can View Employees Absent Today</p>
+          <p class="section-lead">You can view employees absent Today</p>
           <div class="row">
             <div class="col-12">
               <div class="card">
                 <div class="card-header">
                   <h4>All Employees Absent Today</h4>
-
                 </div>
                 <div class="card-body">
-					<div class="table-responsive">
-						<table id="datatable-buttons" class="table table-bordered table-striped table-md">
-							<thead>
-							<tr>
-								<th>Employee Name</th>
-								<th>Employee Unique Id</th>
-								<th>Enrollment Status</th>
-
-
-							</tr>
-							</thead>
-							<tbody>
-							<?php
-							$date = date('Y-m-d', time());
-							if(!empty($employees)):
-								foreach($employees as $employee):
-									$check_biometrics  = $CI->biometric->get_employee_biometric($employee->employee_id);
-									if(!empty($check_biometrics)):
-										$check_login = $CI->biometric->check_clock_in($employee->employee_id, $date);
-
-										if(empty($check_login)):
-
-											?>
-											<tr>
-												<td><?php echo $employee->employee_last_name." ".$employee->employee_first_name." ".$employee->employee_other_name; ?></td>
-												<td><?php echo $employee->employee_unique_id; ?></td>
-												<td><?php
-
-													$check_biometrics  = $CI->biometric->get_employee_biometric($employee->employee_id);
-
-													if(empty($check_biometrics)): ?>
-
-														<div class="badge badge-danger">Not Enrolled</div>
-
-													<?php	else: ?>
-														<div class="badge badge-success">Enrolled</div>
-
-													<?php	endif;
-
-
-													?>
-													<code id="<?php echo "user_finger_".$employee->employee_id; ?>"> <?php echo count($check_biometrics); ?></code>
-
-												</td>
-
-
-											</tr>
-										<?php
-										endif;
-									endif;
-
-								endforeach;
-							endif; ?>
-							</tbody>
-						</table>
-					</div>
+					        <div class="table-responsive">
+						        <table id="datatable-buttons-2" class="table table-bordered table-striped table-md">
+                      <thead>
+                      <tr>
+                        <th>Employee Name</th>
+                        <th>Employee Unique Id</th>
+                        <th>Enrollment Status</th>
+                      </tr>
+                      </thead>
+							        <tbody>
+                        <?php
+                        $date = date('Y-m-d', time());
+                        if(!empty($employees)):
+                          foreach($employees as $employee):
+                            $check_biometrics  = $CI->biometric->get_employee_biometric($employee->employee_id);
+                            if(!empty($check_biometrics)):
+                              $check_login = $CI->biometric->check_clock_in($employee->employee_id, $date);
+                              if(empty($check_login)):?>
+                                <tr>
+                                  <td><?php echo $employee->employee_last_name." ".$employee->employee_first_name." ".$employee->employee_other_name; ?></td>
+                                  <td><?php echo $employee->employee_unique_id; ?></td>
+                                  <td>
+                                    <?php $check_biometrics  = $CI->biometric->get_employee_biometric($employee->employee_id);
+                                    if(empty($check_biometrics)): ?>
+                                      <div class="badge badge-danger">Not Enrolled</div>
+                                    <?php	else: ?>
+                                      <div class="badge badge-success">Enrolled</div>
+                                    <?php	endif; ?>
+                                    <input type="hidden" id="<?php echo "user_finger_".$employee->employee_id; ?>" value="<?php echo count($check_biometrics); ?>">
+                                  </td>
+                                </tr>
+                              <?php
+                              endif;
+                            endif;
+                          endforeach;
+                        endif; ?>
+                      </tbody>
+                    </table>
+					        </div>
                 </div>
-                <div class="card-footer bg-whitesmoke"></div>
+                <div class="card-footer text-right bg-whitesmoke">
+                  <button class="btn btn-danger" onclick="location.href='<?php echo site_url('biometrics_report');?>'">Go Back</button>
+                </div>
               </div>
             </div>
           </div>
@@ -101,3 +87,6 @@ $CI->load->model('biometric');
 <?php include(APPPATH.'/views/js.php'); ?>
 </body>
 </html>
+<script>
+  $('title').html('Employees Absent Today - IHUMANE');
+</script>
