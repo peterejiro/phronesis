486ac94ff43da6ece4909550310f56a6d609c105
peterejiro <peterejiro96@gmail.com>
2021-01-28 13:56:18 +0100

    leave bug

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -43,7 +43,7 @@ class Home extends CI_Controller
 
 			endforeach;
 
-	 $this->employees->check_leave_end_date(date('Y-m-d'));
+	 //$this->employees->check_leave_end_date(date('Y-m-d'));
 
 		$username = $this->session->userdata('user_username');
 
